713a08711ad16b5e48c446b636fba4c7721479f4
Stan Chen <solve153@gmail.com>
2017-12-02 22:30:11 +0800

    Complete conversion feature

--- src/pages/CtoF.vue
+++ src/pages/CtoF.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="container">
-    <h2>華氏：{{ fahrenheit }} °F</h2>
+    <h1>Temperature Conversion</h1>
+
+    <input id="toggleTemperature" type="checkbox" v-model="isCelsius">
+    <label for="toggleTemperature">切換：{{ toggleTemperature }}</label>
+
+    <h2 v-if="isCelsius">攝氏：{{ celsius }} °C</h2>
+    <h2 v-if="!isCelsius">華氏：{{ fahrenheit }} °F</h2>
+
     <div class="celsius">
-      攝氏：<input type='number' v-model="celsius" /> °C
+      <span v-show="!isCelsius">攝氏：</span>
+      <span v-show="isCelsius">華氏：</span>
+      <input type='number' v-model="userInput" /> °C
     </div>
+
+    <button @click="temperatureConversio( userInput );">
+      Temperature Conversio
+    </button>
   </div>
 </template>
 
@@ -11,12 +24,29 @@
 export default {
   data () {
     return {
-      celsius: 0
+      userInput: 0,
+      isCelsius: false,
     }
   },
   computed: {
     fahrenheit () {
-      return this.celsius * 9/5 + 32;
+      return this.userInput * 9/5 + 32;
+    },
+    celsius () {
+      return (this.userInput - 32) * 5/9;
+    },
+    toggleTemperature () {
+      return this.isCelsius ? "攝氏 轉換 華氏" : "華氏 轉換 攝氏";
+    }
+  },
+  methods: {
+    temperatureConversio ( val ) {
+      if ( this.isCelsius ) {
+        alert('攝氏: ' + this.celsius );
+      }
+      else {
+        alert('華氏: ' + this.fahrenheit );
+      }
     }
   }
 }
@@ -25,5 +55,6 @@ export default {
 <style>
   .celsius {
     font-size: 1.5em;
+    margin-bottom: 1em;
   }
 </style>
