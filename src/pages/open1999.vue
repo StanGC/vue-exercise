6be6c74832dc6c97a0115895b99713130d96e9a7
Stan Chen <solve153@gmail.com>
2018-03-12 21:57:19 +0800

    Add search filter

--- src/pages/open1999.vue
+++ src/pages/open1999.vue
@@ -4,6 +4,30 @@
       <div class="container-fluid">
         <div class="navbar-header">
           <a class="navbar-brand" href="#">open1999</a>
+          <div class="navbar-form navbar-left">
+            <div class="form-group">
+              <label for="searchRegion">搜尋地區：</label>
+              <select id="searchRegion" class="form-control" v-model="searchRegion">
+                <option value='all'>全部</option>
+                <option v-for="option in regionOption" :value="option">
+                  {{ option }}
+                </option>
+              </select>
+            </div>
+            <div class="input-group">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Search"
+                v-model="searchKeyword"
+                @keyup.enter="handleWorkSearch">
+              <div class="input-group-btn">
+                <button class="btn btn-default" @click="handleWorkSearch">
+                  <i class="glyphicon glyphicon-search"></i>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
         <ul class="nav navbar-nav navbar-right">
           <li class="nav-item">
@@ -14,24 +38,34 @@
     </nav>
 
     <hr>
-
-    <!-- card -->
-    <div class="row">
-      <div class="col-md-4 col-sm-6" v-for="item in opendate">
-        <div class="thumbnail">
-          <span class="label label-warning">{{ item.ZipName_ }}</span>
-          <span class="label label-ligth-pink">{{ item.InformDesc_ }}</span>
-          <span class="label label-info">{{ item.UnitName_ }}</span>
-          <div class="caption">
-            <h3>
-              <a :href="getGoogleMap( item.address_ )" target="_blank">{{ item.address_ }}</a>
-            </h3>
-            <p>{{ item.BeforeDesc_ }}</p>
-            <small class="text-muted">反應日期：{{ item.Cre_Date_ }}</small>
+  <!-- if -->
+      <div v-if="opendate.length===0" class="alert alert-info" role="alert">
+        很抱歉！找不要您到的資訊。
+      </div>
+  <!-- else -->
+      <div v-else>
+        <div class="alert alert-success" role="alert">
+          總共查到：{{ opendate.length }} 筆資料。
+        </div>
+        <!-- card -->
+        <div class="row">
+          <div class="col-md-4 col-sm-6" v-for="item in opendate">
+            <div class="thumbnail">
+              <span class="label label-warning">{{ item.ZipName_ }}</span>
+              <span class="label label-ligth-pink">{{ item.InformDesc_ }}</span>
+              <span class="label label-info">{{ item.UnitName_ }}</span>
+              <div class="caption">
+                <h3>
+                  <a :href="getGoogleMap( item.address_ )" target="_blank">{{ item.address_ }}</a>
+                </h3>
+                <p>{{ item.BeforeDesc_ }}</p>
+                <small class="text-muted">反應日期：{{ item.Cre_Date_ }}</small>
+              </div>
           </div>
         </div>
       </div>
     </div>
+  <!-- else end -->
 
   </div>
 </template>
@@ -39,11 +73,18 @@
 <script>
 import { mapGetters, mapActions } from 'vuex';
 export default {
+  data() {
+    return {
+      searchRegion: 'all',
+      searchKeyword: '',
+    }
+  },
   created () {
     this.$store.dispatch('open1999');
   },
   computed: mapGetters({
-    opendate: 'getOpen1999'
+    opendate: 'getOpen1999',
+    regionOption: 'getRegionOption'
   }),
   methods: {
     ...mapActions([
@@ -51,6 +92,15 @@ export default {
     getGoogleMap(address) {
       return `https://www.google.com/maps/place/${address}`;
     },
+    handleWorkSearch () {
+      this.$store.dispatch('opendataSearchKeyword', this.searchKeyword);
+    },
+  },
+  watch: {
+    searchRegion (val) {
+      // console.log(searchRegion);
+      this.$store.dispatch('opendataSearchRegion', val);
+    }
   },
 };
 </script>
